c6e5efecda1794892841e0d30aab80274177053d
Your Name <kinaiatovnakoo@gmail.com>
2026-09-11 01:27:57 +0500

    api  request

--- src/main/java/ApiRequest.java
+++ src/main/java/ApiRequest.java
@@ -5,4 +5,29 @@ public class ApiRequest {
     private final String header;
     private final String body;
 
+    ApiRequest( HttpMethod method, String url, String header, String body){
+
+        this.method = method;
+        this.url = url;
+        this.header = header;
+        this.body = body;
+
+    }
+
+    public HttpMethod getMethod(){
+        return method;
+    }
+
+    public String getUrl(){
+        return url;
+    }
+
+    public String getHeader(){
+        return header;
+    }
+
+    public String getBody(){
+        return body;
+    }
+
 }
